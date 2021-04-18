fdc69f8ddf8e1c7ba94432fc066769faa4d9a87d
tanchik_curly <tetiana.diakun.pz.2019@lpnu.ua>
2021-04-18 11:44:31 +0300

    create status

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
     has_many :comments
+    include Visible
 
     validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 8 }
+    validates :body, presence: true, length: { minimum: 2 }
 end
